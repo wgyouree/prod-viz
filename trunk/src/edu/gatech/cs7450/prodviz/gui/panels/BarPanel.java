e7bd5a521900aa614933caaeb3c14ec3b7a3ff05
wgyouree@gmail.com <wgyouree@gmail.com@3d4e436d-2155-f11a-2758-9bbde363e788>
2011-11-07 21:37:36 +0000

    - added more GUI scaffolding, the view is divided into three areas, a main area in the center, and a bottom panel and a right panel

---
+++ trunk/src/edu/gatech/cs7450/prodviz/gui/panels/BarPanel.java
@@ -0,0 +1,14 @@
+package edu.gatech.cs7450.prodviz.gui.panels;
+
+import java.awt.Color;
+
+import edu.gatech.cs7450.prodviz.gui.BottomPanel;
+
+public class BarPanel extends BottomPanel {
+
+	private static final long serialVersionUID = 1L;
+	
+	public BarPanel() {
+		this.setBackground(Color.GREEN);
+	}
+}
---
+++ trunk/src/edu/gatech/cs7450/prodviz/gui/panels/ControlsPanel.java
@@ -0,0 +1,14 @@
+package edu.gatech.cs7450.prodviz.gui.panels;
+
+import java.awt.Color;
+
+import edu.gatech.cs7450.prodviz.gui.RightPanel;
+
+public class ControlsPanel extends RightPanel {
+
+	private static final long serialVersionUID = 1L;
+	
+	public ControlsPanel() {
+		this.setBackground(Color.YELLOW);
+	}
+}
